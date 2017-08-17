5458dadc27545d1b51ceba2095a90141f6b0283c
Doug Goldstein <doug@starlab.io>
2017-08-17 16:28:35 -0500

    examples: use tpm2-tools env vars for conn info
    Use the same environment variables that tpm2-tools use to change how we
    connect.

--- examples/ownership.rs
+++ examples/ownership.rs
@@ -3,6 +3,7 @@ extern crate error_chain;
 extern crate pretty_env_logger;
 extern crate tss_sapi;
 
+use std::env;
 use tss_sapi::*;
 
 quick_main!(run);
@@ -11,8 +12,14 @@ fn run() -> Result<()> {
 
     pretty_env_logger::init().unwrap();
 
+    let addr = env::var("TPM2TOOLS_SOCKET_ADDRESS").ok();
+    let port = match env::var("TPM2TOOLS_SOCKET_PORT").ok() {
+        None => None,
+        Some(v) => Some(v.parse::<u16>().chain_err(|| "Unable to parse TPM2TOOLS_SOCKET_PORT")?),
+    };
+
     // create a SAPI context and connect to the default TPM emulator
-    let ctx = Context::socket(None, None)?;
+    let ctx = tss_sapi::Context::socket(addr.as_ref().map(|v| v.as_ref()), port)?;
 
     // attempt to take ownership of the TPM with the password 'test123'
     ctx.take_ownership("test123")
