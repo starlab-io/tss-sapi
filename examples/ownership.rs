a8835585fd4d03b5760121b5935e3617c42f02a2
Doug Goldstein <doug@starlab.io>
2017-08-23 11:09:50 -0500

    add ability to connect to the device in the example
    The existing example only allow you to connect to the TPM over a socket,
    which is not how real devices work so add the ability to connect to real
    devices and default to that.

--- examples/ownership.rs
+++ examples/ownership.rs
@@ -12,14 +12,33 @@ fn run() -> Result<()> {
 
     pretty_env_logger::init().unwrap();
 
-    let addr = env::var("TPM2TOOLS_SOCKET_ADDRESS").ok();
-    let port = match env::var("TPM2TOOLS_SOCKET_PORT").ok() {
-        None => None,
-        Some(v) => Some(v.parse::<u16>().chain_err(|| "Unable to parse TPM2TOOLS_SOCKET_PORT")?),
-    };
+    // assume the device if the environment variable was not supplied
+    let tcti = env::var("TPM2TOOLS_TCTI_NAME").unwrap_or_else(|_| String::from("device"));
 
-    // create a SAPI context and connect to the default TPM emulator
-    let ctx = tss_sapi::Context::socket(addr.as_ref().map(|v| v.as_ref()), port)?;
+    let ctx = match tcti.as_str() {
+        "socket" => {
+            let addr = env::var("TPM2TOOLS_SOCKET_ADDRESS").ok();
+            let port = match env::var("TPM2TOOLS_SOCKET_PORT").ok() {
+                None => None,
+                Some(v) => {
+                    Some(v.parse::<u16>().chain_err(|| "Unable to parse TPM2TOOLS_SOCKET_PORT")?)
+                }
+            };
+
+            // create a SAPI context and connect to the default TPM emulator
+            tss_sapi::Context::socket(addr.as_ref().map(|v| v.as_ref()), port)
+                .chain_err(|| format!("Unable to connect to {:?}:{:?}", addr, port))
+        }
+        "device" => {
+            let dev = env::var("TPM2TOOLS_DEVICE_FILE").ok();
+
+            // create a SAPI context and connect to the
+            // device requested or the default device
+            tss_sapi::Context::device(dev.as_ref().map(|v| v.as_ref()))
+                .chain_err(|| format!("Unable to connect to {:?}", dev))
+        }
+        _ => Err(ErrorKind::Msg("invalid TPM2TOOLS_TCTI_NAME value".into()).into()),
+    }?;
 
     // check if the TPM is already owned
     if ctx.is_owned()? {
