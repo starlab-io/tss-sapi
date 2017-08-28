ab5039fc33ea9a65d4a6a431e9e25e1e567e573a
Doug Goldstein <doug@starlab.io>
2017-08-28 14:29:53 -0500

    examples: make tcti selection shared
    Decide which TCTI to use and how to initialize it from environment
    variables just like the official TPM2.0 tools from Intel. You can't
    use modules inside of examples so use an include so we can have
    shared code.

--- examples/ownership.rs
+++ examples/ownership.rs
@@ -3,8 +3,10 @@ extern crate error_chain;
 extern crate pretty_env_logger;
 extern crate tss_sapi;
 
-use std::env;
 use tss_sapi::*;
+
+// can't use modules in examples
+include!("tcti.rsinclude");
 
 quick_main!(run);
 
@@ -12,33 +14,8 @@ fn run() -> Result<()> {
 
     pretty_env_logger::init().unwrap();
 
-    // assume the device if the environment variable was not supplied
-    let tcti = env::var("TPM2TOOLS_TCTI_NAME").unwrap_or_else(|_| String::from("device"));
-
-    let ctx = match tcti.as_str() {
-        "socket" => {
-            let addr = env::var("TPM2TOOLS_SOCKET_ADDRESS").ok();
-            let port = match env::var("TPM2TOOLS_SOCKET_PORT").ok() {
-                None => None,
-                Some(v) => {
-                    Some(v.parse::<u16>().chain_err(|| "Unable to parse TPM2TOOLS_SOCKET_PORT")?)
-                }
-            };
-
-            // create a SAPI context and connect to the default TPM emulator
-            tss_sapi::Context::socket(addr.as_ref().map(|v| v.as_ref()), port)
-                .chain_err(|| format!("Unable to connect to {:?}:{:?}", addr, port))
-        }
-        "device" => {
-            let dev = env::var("TPM2TOOLS_DEVICE_FILE").ok();
-
-            // create a SAPI context and connect to the
-            // device requested or the default device
-            tss_sapi::Context::device(dev.as_ref().map(|v| v.as_ref()))
-                .chain_err(|| format!("Unable to connect to {:?}", dev))
-        }
-        _ => Err(ErrorKind::Msg("invalid TPM2TOOLS_TCTI_NAME value".into()).into()),
-    }?;
+    // use function from tcti.rs
+    let ctx = open_context()?;
 
     // check if the TPM is already owned
     if ctx.is_owned()? {
