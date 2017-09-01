20a6ccfc684d1be9883b8e828662233f8d3862cf
Doug Goldstein <doug@starlab.io>
2017-08-31 21:42:55 -0500

    provide conversion of NvAttribtes to FFI type
    The SAPI takes TPMA_NV while we are representing it as NvAttributes.
    Before this change you could go only in one direction and now it is
    bi-directional. Provide a simple way to create a NvAttribute to be
    converted to a TPMA_NV by using derive_builder to provide the builder
    pattern over the various booleans.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@
 #![recursion_limit = "1024"]
 
 #[macro_use]
+extern crate derive_builder;
+#[macro_use]
 extern crate enum_primitive_derive;
 #[macro_use]
 extern crate error_chain;
@@ -130,6 +132,44 @@ mod sys {
     impl From<TPMS_AUTH_RESPONSE> for TSS2_SYS_RSP_AUTHS {
         fn from(resp: TPMS_AUTH_RESPONSE) -> Self {
             TSS2_SYS_RSP_AUTHS::new(vec![resp]).unwrap()
+        }
+    }
+
+    macro_rules! nv_attrs(
+        ($field:expr, $save:ident, $val:path) => (
+            if $field {
+                $save.bindgen_union_field += $val;
+            }
+            )
+        );
+
+    impl From<super::NvAttributes> for TPMA_NV {
+        fn from(attrs: super::NvAttributes) -> Self {
+            let mut built = TPMA_NV::default();
+
+            nv_attrs!(attrs.ppread, built, TPMA_NV_TPMA_NV_PPREAD);
+            nv_attrs!(attrs.ppwrite, built, TPMA_NV_TPMA_NV_PPWRITE);
+            nv_attrs!(attrs.owner_read, built, TPMA_NV_TPMA_NV_OWNERREAD);
+            nv_attrs!(attrs.owner_write, built, TPMA_NV_TPMA_NV_OWNERWRITE);
+            nv_attrs!(attrs.auth_read, built, TPMA_NV_TPMA_NV_AUTHREAD);
+            nv_attrs!(attrs.auth_write, built, TPMA_NV_TPMA_NV_AUTHWRITE);
+            nv_attrs!(attrs.policy_read, built, TPMA_NV_TPMA_NV_POLICYREAD);
+            nv_attrs!(attrs.policy_write, built, TPMA_NV_TPMA_NV_POLICYWRITE);
+            nv_attrs!(attrs.policy_delete, built, TPMA_NV_TPMA_NV_POLICY_DELETE);
+            nv_attrs!(attrs.read_locked, built, TPMA_NV_TPMA_NV_READLOCKED);
+            nv_attrs!(attrs.write_locked, built, TPMA_NV_TPMA_NV_WRITELOCKED);
+            nv_attrs!(attrs.written, built, TPMA_NV_TPMA_NV_WRITTEN);
+            nv_attrs!(attrs.write_all, built, TPMA_NV_TPMA_NV_WRITEALL);
+            nv_attrs!(attrs.write_define, built, TPMA_NV_TPMA_NV_WRITEDEFINE);
+            nv_attrs!(attrs.read_stclear, built, TPMA_NV_TPMA_NV_READ_STCLEAR);
+            nv_attrs!(attrs.write_stclear, built, TPMA_NV_TPMA_NV_WRITE_STCLEAR);
+            nv_attrs!(attrs.clear_stclear, built, TPMA_NV_TPMA_NV_CLEAR_STCLEAR);
+            nv_attrs!(attrs.global_lock, built, TPMA_NV_TPMA_NV_GLOBALLOCK);
+            nv_attrs!(attrs.no_da, built, TPMA_NV_TPMA_NV_NO_DA);
+            nv_attrs!(attrs.orderly, built, TPMA_NV_TPMA_NV_ORDERLY);
+            nv_attrs!(attrs.platform_create, built, TPMA_NV_TPMA_NV_PLATFORMCREATE);
+
+            built
         }
     }
 
@@ -360,7 +400,7 @@ pub enum TpmAlgorithm {
     ECB = sys::TPM_ALG_ECB as isize,
 }
 
-#[derive(Debug)]
+#[derive(Builder, Clone, Debug, Eq, PartialEq)]
 pub struct NvAttributes {
     pub ppread: bool,
     pub ppwrite: bool,
